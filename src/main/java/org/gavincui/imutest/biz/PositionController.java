a0f36fba6f8da7f94c282448d3e08a240c47fe72
Gavin Cui <clarkhillm@gmail.com>
2022-12-12 22:13:39 +0800

    update ...

--- src/main/java/org/gavincui/imutest/biz/PositionController.java
+++ src/main/java/org/gavincui/imutest/biz/PositionController.java
@@ -1,7 +1,11 @@
 package org.gavincui.imutest.biz;
 
+import java.util.List;
+
 import org.gavincui.imutest.dao.PositionDao;
 import org.gavincui.imutest.dao.model.Position;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +15,9 @@ import io.swagger.annotations.Api;
 @RestController
 @RequestMapping(value = "/imu/position")
 public class PositionController extends AbstractBaseController<PositionDao, Position> {
+
+    @GetMapping("/{lineId}")
+    List<Position> getByLine(@PathVariable String lineId) {
+        return dao.queryByLineId(lineId);
+    }
 }
--- src/main/java/org/gavincui/imutest/dao/PositionDao.java
+++ src/main/java/org/gavincui/imutest/dao/PositionDao.java
@@ -1,10 +1,17 @@
 package org.gavincui.imutest.dao;
 
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
 import org.gavincui.imutest.dao.model.Position;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 @Repository
 public interface PositionDao extends JpaRepository<Position, String> {
+
+    @Query("select obj from Position obj where obj.lineId=?1")
+    List<Position> queryByLineId(String lineId);
 
 }
